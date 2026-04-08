c97d8eb0c5037e5db4792e3f598a1c7a400b97b7
김근호 <insertvalue@naver.com>
2026-04-08 10:33:05 +0900

    view 1차 코드 구현

--- src/com/tenco/library/util/DatabaseUtil.java
+++ src/com/tenco/library/util/DatabaseUtil.java
@@ -12,31 +12,10 @@ public class DatabaseUtil {
 
     // 새로운 DB 연결 객체를 반환 합니다.
     public static Connection getConnection() throws SQLException {
-        // 재미삼아 효과 만들어 보기
-        Thread thread = new Thread(() -> {
-            System.out.print("Connecting to database");
-            for (int i = 0; i < 5; i++) {
-                System.out.print(".");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
         Connection connection = DriverManager.getConnection(URL, DB_USER, PASSWORD);
         System.out.println();
-        System.out.println(connection.getMetaData().getDatabaseProductName());
-        System.out.println(connection.getMetaData().getDatabaseProductVersion());
-
+//        System.out.println(connection.getMetaData().getDatabaseProductName());
+//        System.out.println(connection.getMetaData().getDatabaseProductVersion());
         return connection;
     }
 
